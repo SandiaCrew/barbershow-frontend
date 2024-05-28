a28f5d58fb02da782b6a564c4ac350ca2351cc96
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 00:35:21 +0200

    Create ScanQRPage component

---
+++ src/pages/ScanQRPage.js
@@ -0,0 +1,12 @@
+// src/pages/ScanQRPage.js
+import React from 'react';
+import QRScanner from '../components/QRScanner';
+
+const ScanQRPage = () => (
+  <main>
+    <h1>Scan QR Code</h1>
+    <QRScanner />
+  </main>
+);
+
+export default ScanQRPage;
