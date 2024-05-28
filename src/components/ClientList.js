9655418061788ce094e810e608bb2fbda67b696b
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 01:21:14 +0200

    Create ClientList component

---
+++ src/components/ClientList.js
@@ -0,0 +1,25 @@
+// src/components/ClientList.js
+import React from 'react';
+
+const ClientList = () => (
+  <ul className="client-list">
+    {/* Placeholder for dynamically generated client list items */}
+    <li className="client-list__item">
+      <a href="client.html" className="client--link">Client Name</a>
+    </li>
+    <li className="client-list__item">
+      <a href="client.html" className="client--link">Client Name</a>
+    </li>
+    <li className="client-list__item">
+      <a href="client.html" className="client--link">Client Name</a>
+    </li>
+    <li className="client-list__item">
+      <a href="client.html" className="client--link">Client Name</a>
+    </li>
+    <li className="client-list__item">
+      <a href="client.html" className="client--link">Client Name</a>
+    </li>
+  </ul>
+);
+
+export default ClientList;
