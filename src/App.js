625fc6549323b1c3659f6b1d1802462d4e9d901a
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 00:37:17 +0200

    Integrate ScanQRPage into App component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
+import ScanQRPage from './pages/ScanQRPage';
 import './App.css';
 
 function App() {
@@ -13,7 +14,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
+<<<<<<< HEAD
           {/* Placeholder for other routes */}
+=======
+          <Route path="/scan-qr" element={<ScanQRPage />} />
+>>>>>>> 798d8e2 (Integrate ScanQRPage into App component)
         </Routes>
         <Footer />
       </div>
