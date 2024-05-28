118f5fd31451dd5d7790a853d70d9fad950a5b49
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 01:19:57 +0200

    Create SearchBar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,8 @@
+// src/components/SearchBar.js
+import React from 'react';
+
+const SearchBar = () => (
+  <input type="text" className="search-bar" placeholder="Search..." />
+);
+
+export default SearchBar;
