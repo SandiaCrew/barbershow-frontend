37faf2761388d9d3711155737e045dc463db062f
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 01:01:03 +0200

    Create SettingsPage component and integrate Navbar, RewardForm, and RewardsList

---
+++ src/pages/SettingsPage.js
@@ -0,0 +1,23 @@
+// src/pages/SettingsPage.js
+import React from 'react';
+import Navbar from '../components/Navbar';
+import RewardForm from '../components/RewardForm';
+import RewardsList from '../components/RewardsList';
+
+const SettingsPage = () => (
+  <div className="main">
+    <div className="container container--page">
+      <Navbar />
+      <div className="container container-body">
+        <h1 className="page-title">Settings</h1>
+        <div className="rewards-settings">
+          <h3>Loyalty Rewards</h3>
+          <RewardForm />
+          <RewardsList />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default SettingsPage;
