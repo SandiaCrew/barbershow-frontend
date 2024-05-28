45fd40511bc35541cfba3e49d64d8c14c9a79cd5
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 01:09:55 +0200

    Update App.js to use new HomePage component

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/scan-qr" element={<ScanQRPage />} />
