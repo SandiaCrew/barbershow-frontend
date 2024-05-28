397faf27c8709c0d193f773cb9ac97a4e8ea2c4c
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 00:18:02 +0200

    Create Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+// src/components/Footer.js
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <p>© 2024 Barber Show. All rights reserved.</p>
+  </footer>
+);
+
+export default Footer;
