0170742b1c236754f6920e12c33c3b7bfc9e18ec
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 00:36:27 +0200

    Create QRScanner component

---
+++ src/components/QRScanner.js
@@ -0,0 +1,10 @@
+// src/components/QRScanner.js
+import React from 'react';
+
+const QRScanner = () => (
+  <div className="qr-scanner">
+    <img src="assets/placeholder-qr.webp" alt="QR Code Placeholder" />
+  </div>
+);
+
+export default QRScanner;
