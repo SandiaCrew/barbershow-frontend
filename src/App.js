1e0d44291b801803f7b3934bdf37c437761197cb
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 00:17:10 +0200

    Integrate HomeNavbar into App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import HomeNavbar from './components/HomeNavbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomeNavbar />
+      <div className="content">
+        {/* Main content will go here */}
+      </div>
     </div>
   );
 }
