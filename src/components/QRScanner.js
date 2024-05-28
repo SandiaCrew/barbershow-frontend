3dfe4c24b2f1995e7a3917e3eeb17d2004f36fd7
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 01:13:42 +0200

    Create QRScanner component

--- src/components/QRScanner.js
+++ src/components/QRScanner.js
@@ -2,8 +2,14 @@
 import React from 'react';
 
 const QRScanner = () => (
-  <div className="qr-scanner">
-    <img src="assets/placeholder-qr.webp" alt="QR Code Placeholder" />
+  <div className="qr-code-scanner">
+    {/* QR code scanner functionality will be implemented here */}
+    {/* Scanner needs to activate the phone's back camera, NOT THE FRONT. */}
+    {/* Once the QR code is scanned, it logs the client's visit to the barber shop as a date (time not needed, only date). */}
+    {/* That visit is logged into the database with that client. */}
+    {/* Visits serve for the Loyalty program, to track how many visits the client had. */}
+    {/* They will all be shown on their page. */}
+    <p>QR code scanner functionality will be here</p>
   </div>
 );
 
