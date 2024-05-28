fab1f57af18c4a53207548e4736d21a7196c5980
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 01:17:27 +0200

    Update App.js to use new ScanQRPage component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import ScanQRPage from './pages/ScanQRPage';
 import SettingsPage from './pages/SettingsPage';
@@ -12,13 +10,11 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/scan-qr" element={<ScanQRPage />} />
           <Route path="/settings" element={<SettingsPage />} />
         </Routes>
-        <Footer />
       </div>
     </Router>
   );
