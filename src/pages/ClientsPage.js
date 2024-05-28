ce71980ca3d7a5fd90065397cfa7c576467726a0
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 01:22:30 +0200

    Create ClientsPage component and integrate Navbar, SearchBar, and ClientList

---
+++ src/pages/ClientsPage.js
@@ -0,0 +1,20 @@
+// src/pages/ClientsPage.js
+import React from 'react';
+import Navbar from '../components/Navbar';
+import SearchBar from '../components/SearchBar';
+import ClientList from '../components/ClientList';
+
+const ClientsPage = () => (
+  <div className="main">
+    <div className="container container--page">
+      <Navbar />
+      <div className="container container-body">
+        <h1 className="page-title">Clients</h1>
+        <SearchBar />
+        <ClientList />
+      </div>
+    </div>
+  </div>
+);
+
+export default ClientsPage;
