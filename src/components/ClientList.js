60736555a4a12e1ebd287e1536e12f5e78038dd6
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 14:14:11 +0200

    Modify ClientList component to fetch and display clients dynamically

--- src/components/ClientList.js
+++ src/components/ClientList.js
@@ -1,25 +1,31 @@
-// src/components/ClientList.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from '../axios';
 
-const ClientList = () => (
-  <ul className="client-list">
-    {/* Placeholder for dynamically generated client list items */}
-    <li className="client-list__item">
-      <a href="client.html" className="client--link">Client Name</a>
-    </li>
-    <li className="client-list__item">
-      <a href="client.html" className="client--link">Client Name</a>
-    </li>
-    <li className="client-list__item">
-      <a href="client.html" className="client--link">Client Name</a>
-    </li>
-    <li className="client-list__item">
-      <a href="client.html" className="client--link">Client Name</a>
-    </li>
-    <li className="client-list__item">
-      <a href="client.html" className="client--link">Client Name</a>
-    </li>
-  </ul>
-);
+const ClientList = () => {
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+    const fetchClients = async () => {
+      try {
+        const response = await axios.get('/clients');
+        setClients(response.data);
+      } catch (error) {
+        console.error('Error fetching clients:', error);
+      }
+    };
+
+    fetchClients();
+  }, []);
+
+  return (
+    <ul className="client-list">
+      {clients.map(client => (
+        <li key={client._id} className="client-list__item">
+          <a href={`client.html?id=${client._id}`} className="client--link">{client.name}</a>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default ClientList;
