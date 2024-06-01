21506e65633609880064d748936ebb777bec9029
Sandia <sandiacreativecrew@gmail.com>
2024-06-01 19:42:45 +0200

    Removed the notice message on the client detail page because it is not necessary

--- src/components/VisitList.js
+++ src/components/VisitList.js
@@ -43,9 +43,6 @@ const VisitList = ({ clientId }) => {
     <div className="visits">
       <h3>Visits</h3>
       <span className="total-visits">Total Visits<span>{visits.length}</span></span>
-      {visits.length > 0 && (
-        <span className="notice notice--active">There are rewards to be collected!</span>
-      )}
       <ul className="visit-list">
         {visits.map((visit, index) => (
           <li key={index} className="visit-list-item">
