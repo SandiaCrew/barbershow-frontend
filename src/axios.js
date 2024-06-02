5b737f8d2f7345d675b42d77ec5c49cff9377c69
Sandia <sandiacreativecrew@gmail.com>
2024-06-03 00:09:19 +0200

    idk anymore

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+console.log('API URL:', process.env.REACT_APP_API_URL); // Add this line
+
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const instance = axios.create({
-  baseURL: apiUrl
+  baseURL: apiUrl,
 });
 
 export default instance;
